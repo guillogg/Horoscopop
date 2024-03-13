17731d7133844e85a453abac4e2cec5ac0af7506
guillo <guillermo_gaona@hotmail.com>
2024-03-13 11:06:17 -0300

    Colors

---
+++ app/src/main/java/com/example/horoscopop/ui/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.horoscopop.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.horoscopop.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
